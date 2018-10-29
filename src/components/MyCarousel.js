86c3a2efb772971eb21140ee1eef8825b3d91d5a
jabusir <jabusir@gmail.com>
2018-10-28 19:17:45 -0700

    Fix carousel bug

--- src/components/MyCarousel.js
+++ src/components/MyCarousel.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 export default class MyCarousel extends React.Component {
     render() {
         return (
@@ -8,6 +10,9 @@ export default class MyCarousel extends React.Component {
                 <Carousel showArrows={true}>
                     <div> 
                         <img src="sample.jpg" alt="patrick" height="400" width="400" />
+                    </div>
+                    <div> 
+                        <img src="sample2.jpeg" alt="sponge" height="400" width="400" />
                     </div>
                 </Carousel>
             </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/projects.css';
-import MyCarousel from './Carousel';
+import MyCarousel from './MyCarousel';
 
 const Projects =() => (
     <div className="projects"> 
