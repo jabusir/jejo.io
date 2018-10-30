bab09a7b9d180c79969e7186760f42acd03abed8
jabusir <jabusir@gmail.com>
2018-10-29 20:27:09 -0700

    Final commit for the day

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,14 +6,9 @@ const AboutMe = () => (
         <div className="about-me">
             <div className="about-me__header">Hi, I'm Jehad, but my dev team calls me Jejo</div>
             <div className="about-me__header">I'm a Software Engineer looking for new and exciting opportunites to perpetually refine my chunks</div>
-            <div className="format-container">
                 <div className="about-me__body">
                     <div>Currently, I'm in the final year of finishing my Bachelor's in Economics with an emphasis in Data Analysis, along with a minor in Computer Science</div>
                 </div>
-                <div className="skills-container">
-                    <div className="skills-header">This is what I know</div>    
-                </div>
-            </div>
             </div>
             <div className="footer">Here are some projects I've worked on in the meantime...</div>
     </div>
