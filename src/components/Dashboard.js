b935208d6412ea5d85ff4b777053a5a8e35c1d6a
Jehad <jabusir@gmail.com>
2020-03-28 18:27:28 -0700

    finish frontend for mailer

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import AboutMe from './AboutMe';
-import Projects from './Projects';
-import Footer from './Footer';
-import Skills from './Skills';
-
-import '../styles/base.css';
+import React, { Component } from "react";
+import Header from "./Header";
+import AboutMe from "./AboutMe";
+import Projects from "./Projects";
+import Footer from "./Footer";
+import Skills from "./Skills";
+import Mailer from "./Mailer";
+import "../styles/base.css";
 
 class Dashboard extends Component {
   render() {
@@ -13,10 +13,11 @@ class Dashboard extends Component {
       <div className="dashboard-container">
         <Header />
         <div className="dashboard">
-            <AboutMe />
-            <Skills />
-            <Projects />
-            <Footer />
+          <AboutMe />
+          <Skills />
+          <Projects />
+          <Mailer />
+          <Footer />
         </div>
       </div>
     );
